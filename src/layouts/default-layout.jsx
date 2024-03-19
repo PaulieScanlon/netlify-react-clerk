05cff61ca431ffbd8509e2f7e34d1b888e54359c
Paul Scanlon <pauliescanlon@gmail.com>
2024-03-19 18:12:38 -0500

    feat: add all routes and deps

---
+++ src/layouts/default-layout.jsx
@@ -0,0 +1,45 @@
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { ClerkProvider, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+
+const Layout = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ClerkProvider
+      navigate={navigate}
+      publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      afterSignInUrl='/dashboard'
+      afterSignUpUrl='/dashboard'
+    >
+      <header>
+        <p>netlify-react-clerk</p>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/dashboard'>Dashboard</Link>
+          </li>
+          <SignedIn>
+            <li>
+              <UserButton afterSignOutUrl='/' />
+            </li>
+          </SignedIn>
+          <SignedOut>
+            <li>
+              <Link to='/sign-in'>Sign In</Link>
+            </li>
+            <li>
+              <Link to='/sign-up'>Sign Up</Link>
+            </li>
+          </SignedOut>
+        </ul>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </ClerkProvider>
+  );
+};
+
+export default Layout;
